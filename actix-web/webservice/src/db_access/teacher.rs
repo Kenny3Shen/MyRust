e3d08974407694f801ec70180b691b45ba07e0b0
Kenny3Shen <shen.ss@outlook.com>
2023-07-18 13:24:12 +0800

    2023/7/18

--- actix-web/webservice/src/db_access/teacher.rs
+++ actix-web/webservice/src/db_access/teacher.rs
@@ -11,7 +11,7 @@ pub async fn get_all_teachers_db(pool: &PgPool) -> Result<Vec<Teacher>, MyError>
         .iter()
         .map(|r| Teacher {
             id: r.id,
-            name: r.name.clone(),
+            name: r.name.clone().unwrap_or_default(),
             picture_url: r.picture_url.clone(),
             profile: r.profile.clone(),
         })
@@ -29,7 +29,7 @@ pub async fn get_teacher_details_db(pool: &PgPool, teacher_id: i32) -> Result<Te
         .await
         .map(|r| Teacher {
             id: r.id,
-            name: r.name.clone(),
+            name: r.name.clone().unwrap_or_default(),
             picture_url: r.picture_url.clone(),
             profile: r.profile.clone(),
         })
@@ -54,7 +54,7 @@ pub async fn post_new_teacher_db(
 
     Ok(Teacher {
         id: row.id,
-        name: row.name.clone(),
+        name: row.name.clone().unwrap_or_default(),
         picture_url: row.picture_url.clone(),
         profile: row.profile.clone(),
     })
@@ -73,7 +73,7 @@ pub async fn update_teacher_details_db(
     let name = if let Some(name) = update_teacher.name {
         name
     } else {
-        row.name
+        row.name.unwrap_or_default()
     };
     let picture_url = if let Some(picture_url) = update_teacher.picture_url {
         picture_url
@@ -98,7 +98,7 @@ pub async fn update_teacher_details_db(
     .await
     .map(|row| Teacher {
         id: row.id,
-        name: row.name.clone(),
+        name: row.name.clone().unwrap_or_default(),
         picture_url: row.picture_url.clone(),
         profile: row.profile.clone(),
     })
--- actix-web/webservice/src/main.rs
+++ actix-web/webservice/src/main.rs
@@ -1,10 +1,3 @@
-mod handlers;
-mod models;
-mod routers;
-mod state;
-mod db_access;
-mod errors;
-
 fn main() {
     println!("Hello, world!");
 }
