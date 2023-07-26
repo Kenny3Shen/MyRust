d1be2f2fb69ef73f5abafa42defc108ae7abdbed
Kenny3Shen <shen.ss@outlook.com>
2023-07-27 01:11:15 +0800

    2023/7/27 [Rust]

--- rustlings/exercises/clippy/target/debug/deps/clippy3-9d09d43681b2778b.d
+++
@@ -1,8 +0,0 @@
-D:\MyRust\rustlings\exercises\clippy\target\debug\deps\clippy3-9d09d43681b2778b.rmeta: clippy3.rs Cargo.toml
-
-D:\MyRust\rustlings\exercises\clippy\target\debug\deps\clippy3-9d09d43681b2778b.d: clippy3.rs Cargo.toml
-
-clippy3.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
