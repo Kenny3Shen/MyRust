a1fd8f33beba6ce99027110754656aa8ad68a8a9
Kenny3Shen <shen.ss@outlook.com>
2023-08-22 17:57:07 +0800

    2023/8/22

--- axum-websocket-chat/src/handler.rs
+++ axum-websocket-chat/src/handler.rs
@@ -22,16 +22,20 @@ pub async fn handler(
 async fn handle_socket(socket: WebSocket, app_state: AppState, addr: SocketAddr) {
     let (sender, receiver) = socket.split();
 
+    // 将接收到的消息放入发送队列
     app_state.clients.insert(addr.port(), sender);
 
     // let (sender_tx, sender_rx) = mpsc::channel::<Message>(1000);
 
+    // 创建一个发送消息的线程
     // tokio::spawn(write(sender, sender_rx));
     tokio::spawn(read(receiver, app_state, addr.port()));
 }
 
 async fn read(mut receiver: SplitStream<WebSocket>, app_state: AppState, client_port: u16) {
+    // 循环接收到的消息
     while let Some(Ok(msg)) = receiver.next().await {
+        // 将消息转换为 json 格式
         match msg {
             Message::Text(text) => {
                 println!("{text}");
@@ -39,16 +43,20 @@ async fn read(mut receiver: SplitStream<WebSocket>, app_state: AppState, client_
                 let mut payload = serde_json::from_str::<MessagePayLoad>(&text).unwrap();
                 match payload.msg_type {
                     MsgType::Message => {
+                        // 将消息发送到指定的客户端
                         payload.from = Some(client_port);
                         app_state.sender_tx.send(payload).await.unwrap();
                     }
                     MsgType::Command => {
+                        // 如果消息类型为 Command，则检查消息数据是否为 list
                         if payload.data == "list" {
+                            // 获取所有客户端的 key
                             let clients = app_state
                                 .clients
                                 .iter()
                                 .map(|x| x.key().clone())
-                                .collect::<Vec<u16>>();
+                                .collect::<Vec<u16>>();/*  */
+                            // 将 key 和消息发送到指定的客户端
                             let reply = MessagePayLoad {
                                 msg_type: MsgType::Reply,
                                 from: None,
@@ -65,5 +73,6 @@ async fn read(mut receiver: SplitStream<WebSocket>, app_state: AppState, client_
             _ => {}
         }
     }
+    // 删除客户端
     app_state.clients.remove(&client_port);
 }
--- axum-websocket-chat/src/main.rs
+++ axum-websocket-chat/src/main.rs
@@ -14,29 +14,39 @@ use serde_json::json;
 use std::{net::SocketAddr, sync::Arc};
 use tokio::sync::mpsc::{self};
 
+// 定义 Result 类型
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // 创建一个发送消息的通道
     let (sender_tx, mut sender_rx) = mpsc::channel::<MessagePayLoad>(1000);
 
+    // 创建一个应用状态
     let app_state = AppState {
         // clients: Arc::new(RwLock::new(
         //     HashMap::<u16, SplitSink<WebSocket, Message>>::new(),
         // )),
+        // 创建一个 Arc 实例，用于保存客户端连接
         clients: Arc::new(DashMap::<u16, SplitSink<WebSocket, Message>>::new()),
         sender_tx,
     };
 
+    // 创建一个路由
     let app = Router::new()
         .route("/", get(handler))
         .with_state(app_state.clone());
 
     // sender_task
+    // 创建一个发送任务
     let mut sender_task = tokio::spawn(async move {
+        // 遍历发送消息的消息
         while let Some(payload) = sender_rx.recv().await {
+            // 如果消息的接收者存在
             if let Some(to) = payload.to {
+                // 如果消息的发送者存在
                 if let Some(mut sender) = app_state.clients.get_mut(&to) {
+                    // 发送消息
                     sender
                         .value_mut()
                         .send(Message::Text(json!(payload).to_string()))
@@ -48,7 +58,9 @@ async fn main() -> Result<()> {
     });
 
     //server_task
+    // 创建一个服务器任务
     let mut server_task = tokio::spawn(async move {
+        // 绑定端口 3000
         axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
             .serve(app.into_make_service_with_connect_info::<SocketAddr>())
             .await
@@ -57,19 +69,27 @@ async fn main() -> Result<()> {
 
     tokio::select! {
             _ = (&mut sender_task) => {
+                // 打印 Sender Exited
                 println!("Sender Exited");
+                // 取消发送任务
                 server_task.abort();
             },
             _ = (&mut server_task) => {
+                // 打印 Sender Exited
                 println!("Sender Exited");
+                // 取消发送任务
                 sender_task.abort();
             },
             _ = tokio::signal::ctrl_c() => {
+                // 打印 Ctrl + C Received, Exited
                 println!("Ctrl + C Received, Exited");
+                // 取消发送任务
                 sender_task.abort();
+                // 取消服务器任务
                 server_task.abort();
         }
     }
 
+    // 返回结果
     Ok(())
 }
